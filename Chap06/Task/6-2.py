8178e5324128d5069247dffd934a606ac8b71c19
cremecottom <pilotechen@gmail.com>
2025-10-26 16:44:49 +0800

    Update Chpa06

---
+++ Chap06/Task/6-2.py
@@ -0,0 +1,9 @@
+fav_num = {
+    'Jason':10,
+    'Clay':100,
+    'Ray':888,
+    'Sonny':1000,
+    'Stealla':120
+}
+for name,num in fav_num.items():
+    print(f'{name}最喜欢的数字是{num}')
---
+++ Chap06/Task/6-3.py
@@ -0,0 +1,9 @@
+topics = {
+    'Database':'数据库',
+    'Vue':'前端框架',
+    'Spring':'后端框架',
+    'Https':'一种网络协议',
+    'VSCode':'一款强大的工具'
+}
+for name,description in topics.items():
+    print(f'{name}的中文解释是{description}')
